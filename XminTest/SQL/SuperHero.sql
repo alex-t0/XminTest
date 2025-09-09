0231e0efcab6ba830df26d526df503e59033de9a
Alex T <someone@example.com>
2025-09-09 21:26:47 +0300

    sql fix

--- XminTest/SQL/SuperHero.sql
+++ XminTest/SQL/SuperHero.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public."SuperHero" (
                                     "Id" int8 NOT NULL,
                                     "Name" varchar NOT NULL,
-                                    CONSTRAINT superhero_pk PRIMARY KEY (id)
+                                    CONSTRAINT superhero_pk PRIMARY KEY ("Id")
 );
